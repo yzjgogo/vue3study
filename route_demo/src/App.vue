b8acf0b30650fba851fa1e6ed1f0814373c65f03
yinzhijie <yzjgogo@139.com>
2024-06-20 13:58:00 +0800

    提交修改

--- route_demo/src/App.vue
+++ route_demo/src/App.vue
@@ -3,6 +3,7 @@
     <div>我是APP</div>
       <router-link to="/home">首页&nbsp;&nbsp;</router-link>
       <router-link to="/about">&nbsp;&nbsp;关于</router-link>
+      <router-link to="/user/123">&nbsp;&nbsp;用户</router-link>
       <!-- 路由使用步骤7：在浏览器地址栏改变hash或通过router-link等方式改变路由都会让当前路由在router-view中显示 -->
     <router-view></router-view>
   </div>
