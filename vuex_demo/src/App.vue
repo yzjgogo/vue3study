ea0ff955f1b426e6058f133860b3ec38c67b11a7
yinzhijie <yzjgogo@139.com>
2024-08-01 14:38:26 +0800

    提交修改的代码

--- vuex_demo/src/App.vue
+++ vuex_demo/src/App.vue
@@ -15,7 +15,7 @@
 
   const store = useStore()//类似vue2或选项式API的this.$store
 
-  //直接使用store.state.counter不是响应式的，我们可以通过toRefs让其变为响应式,因为store.state是reactive类型的响应式对象，所以我们可以通过toRefs让其所有的属性都变成ref类型的响应式变量
+  //直接使用store.state.counter不是响应式的，我们可以通过toRefs让其变为响应式,因为stonre.state是reactive类型的响应式对象，所以我们可以通过toRefs让其所有的属性都变成ref类型的响应式变量
   console.log('结果呢：',isReactive(store.state),isRef(store.state))//true,false
   // const counter = store.state.counter//不是响应式
   const {counter} = toRefs(store.state)//让store.state的counter变成响应式的
