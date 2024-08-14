f62500baa0a68c5862a217ecb528e46634437c42
yzjgogo <yzjgogo@139.com>
2024-08-14 12:30:16 +0800

    提交家里的代码

--- pinia_demo/src/App.vue
+++ pinia_demo/src/App.vue
@@ -1,85 +1,118 @@
 <template>
-  
-  <div style="background-color: aqua;">访问store里的state</div>
-  <!-- pinia使用步骤5：使用store里的数据，无需.state -->
-  <div>响应式:counterStore.count：{{ counterStore.count }}</div>
-  <div>响应式:toRefs(counterStore):{{ rCount }}</div>
-  <div>响应式:storeToRefs(counterStore):{{ srCount }}</div>
-  <div>非响应式:{{ noResponseCount }}</div>
-  <button @click="updateCount">改变count</button>
+  <div>
+    <div style="background-color: aqua">访问store里的state</div>
+    <!-- pinia使用步骤5：使用store里的数据，无需.state -->
+    <div>响应式:counterStore.count：{{ counterStore.count }}</div>
+    <div>响应式:toRefs(counterStore):{{ rCount }}</div>
+    <div>响应式:storeToRefs(counterStore):{{ srCount }}</div>
+    <div>非响应式:{{ noResponseCount }}</div>
+    <button @click="updateCount">改变count</button>
 
-  <div style="background-color: aqua;margin-top: 20px;">修改stroe里的state</div>
-  <button @click="doPatch">使用$patch一次性修改多个状态</button>
-  <button @click="doState">使用$state替换state为新的对象</button>
-  <button @click="doReset">使用$reset重置state，回到初始状态</button>
-  <div>{{ userStore.name }},{{ userStore.age }},{{ userStore.level }},{{ userStore.home }},{{ userStore }}</div>
+    <div style="background-color: aqua; margin-top: 20px">
+      修改stroe里的state
+    </div>
+    <button @click="doPatch">使用$patch一次性修改多个状态</button>
+    <button @click="doState">使用$state替换state为新的对象</button>
+    <button @click="doReset">使用$reset重置state，回到初始状态</button>
+    <div>
+      {{ userStore.name }},{{ userStore.age }},{{ userStore.level }},{{
+        userStore.home
+      }},{{ userStore }}
+    </div>
 
-  
+    <div style="background-color: aqua; margin-top: 20px">getters</div>
+    <!-- 无需counterStore.getters.doubleCount -->
+    <div>访问getters的dobuleCount:{{ counterStore.doubleCount }}</div>
+    <div>
+      访问getters的doubleCountAddone:{{ counterStore.doubleCountAddone }}
+    </div>
+    <div>访问getters的getFriendById:{{ counterStore.getFriendById(111) }}</div>
+    <div>访问getters的showMessage:{{ counterStore.showMessage }}</div>
 
-  <div style="background-color: aqua;margin-top: 20px;">getters</div>
-  <!-- 无需counterStore.getters.doubleCount -->
-  <div>访问getters的dobuleCount:{{ counterStore.doubleCount }}</div>
-  <div>访问getters的doubleCountAddone:{{ counterStore.doubleCountAddone }}</div>
-  <div>访问getters的getFriendById:{{ counterStore.getFriendById(111) }}</div>
-  <div>访问getters的showMessage:{{ counterStore.showMessage }}</div>
+
+
+    <div style="background-color: aqua; margin-top: 20px">actions</div>
+    <button @click="doincrement">调用actions的increment让count自增</button>
+    <button @click="doincrementNum">调用actions的incrementNum让count每次增加指定个数</button>
+    <button @click="dofetchHomeMultidata">调用actions的fetchHomeMultidata异步获取数据</button>
+    <button @click="dofetchHomeMultidata2">调用actions的fetchHomeMultidata2异步获取数据</button>
+  </div>
 </template>
 
 <script setup>
-import { isProxy, isReactive, toRefs, isRef } from 'vue'
+import { isProxy, isReactive, toRefs, isRef } from "vue";
 // pinia使用步骤4：引入我们定义的业务store,其实是个函数
-import userCounter from '@/stores/counter'
-import { storeToRefs } from 'pinia';
-const counterStore = userCounter()//返回store,counterStore是reactive类型的响应式对象
+import userCounter from "@/stores/counter";
+import { storeToRefs } from "pinia";
+const counterStore = userCounter(); //返回store,counterStore是reactive类型的响应式对象
 
 //是不是reactive类型：isProxy:true,isReactive:true,isRef:false
-console.log(`是不是reactive类型：isProxy:${isProxy(counterStore)},isReactive:${isReactive(counterStore)},isRef:${isRef(counterStore)}`)
+console.log(
+  `是不是reactive类型：isProxy:${isProxy(counterStore)},isReactive:${isReactive(
+    counterStore
+  )},isRef:${isRef(counterStore)}`
+);
 
 // pinia使用步骤6：结构出store里的state里的某个数据
 //因为counterStore是reactive类型的响应式数据，所以noResponseCount不是响应式的
-const { count: noResponseCount } = counterStore//noResponseCount不是响应式的
-const { count: rCount } = toRefs(counterStore)
-const { count: srCount } = storeToRefs(counterStore)//pinia提供的专门用来把reactive数据转为ref类型的响应式数据，与toRefs类似
+const { count: noResponseCount } = counterStore; //noResponseCount不是响应式的
+const { count: rCount } = toRefs(counterStore);
+const { count: srCount } = storeToRefs(counterStore); //pinia提供的专门用来把reactive数据转为ref类型的响应式数据，与toRefs类似
 
 function updateCount() {
-  counterStore.count++
+  counterStore.count++;
 }
-
-
-
 
 // -----------------------------------------------------------------------------------------
 
-
-
-
-import userStoreBuilder from '@/stores/user';
-const userStore = userStoreBuilder()
+import userStoreBuilder from "@/stores/user";
+const userStore = userStoreBuilder();
 
 //参考：subscribe_state.jpeg
 //https://pinia.vuejs.org/zh/core-concepts/state.html#state
 //当state发生变化时，触发监听
 userStore.$subscribe((mutation, state) => {
-  console.log("监听subscribe：", mutation, state)
-})
+  console.log("监听subscribe：", mutation, state);
+});
 function doPatch() {
   //因为Vue 的响应系统是在初始化组件状态时跟踪对象的属性，而后续动态添加的属性不会自动变为响应式的，因此这个home是新添加的，无法被响应式系统跟踪到，这也是为什么上面的{{ userStore.home }},{{ userStore }}都没有显示出home
   userStore.$patch({
     age: 12,
     level: 99,
-    home: '西航港'
-  })
+    home: "西航港",
+  });
 }
 function doState() {
   //因为Vue 的响应系统是在初始化组件状态时跟踪对象的属性，而后续动态添加的属性不会自动变为响应式的，因此这个home是新添加的，无法被响应式系统跟踪到，这也是为什么上面的{{ userStore.home }},{{ userStore }}都没有显示出home
   //https://pinia.vuejs.org/zh/core-concepts/state.html#state
   //注意这里的$state是赋值一个对象给它，且实际上state并不会被赋值的对象覆盖，而是如果这个home没有，只是给state添加一个新的状态叫home，如果这个home本来已经在state初始时就存在，而是更新这个home状态且是响应式的
   userStore.$state = {
-    home: '成都'
-  }
+    home: "成都",
+  };
 }
 
-function doReset(){
-  userStore.$reset()
+function doReset() {
+  userStore.$reset();
+}
+
+
+
+// ---------------------------actions---------------------------------
+function doincrement(){
+  counterStore.increment()
+}
+function doincrementNum(){
+  counterStore.incrementNum(10)
+}
+function dofetchHomeMultidata(){
+  counterStore.fetchHomeMultidata().then(res => {
+    console.log("响应回来1："+res)
+  })
+}
+function dofetchHomeMultidata2(){
+  counterStore.fetchHomeMultidata2().then(res => {
+    console.log("响应回来2："+res)
+  })
 }
 
 </script>
--- pinia_demo/src/stores/counter.js
+++ pinia_demo/src/stores/counter.js
@@ -5,44 +5,79 @@ import userStoreBuilder from './user'
 const userCounter = defineStore("counter", {
   state: () => ({
     count: 99,
-    friends:[
-      {id:111,name:"why"},
-      {id:112,name:"kobe"},
-      {id:113,name:"james"},
-    ]
+    friends: [
+      { id: 111, name: "why" },
+      { id: 112, name: "kobe" },
+      { id: 113, name: "james" },
+    ],
+    banners: [],
+    recommends: []
   }),
 
-// const userStore = userStoreBuilder()
+  // const userStore = userStoreBuilder()
 
   //getters里的方法参数默认就是state
   //getters类似vue组件的computed
-  getters:{
+  getters: {
     //getters基本使用
-    doubleCount(state){
+    doubleCount(state) {
       return state.count * 2
     },
     //getters里的方法内，可以直接通过this访问getters里的别的方法，这个this就是当前store，就是userCounter(),即this == userCounter(),跟外部使用counterStore一个道理
-    doubleCountAddone(){
+    doubleCountAddone() {
       return this.doubleCount + 1
     },
 
     //getters里的方法可以返回一个函数
-    getFriendById(state){
-      return function(id){
-        for(let i=0;i<state.friends.length;i++){
+    getFriendById(state) {
+      return function (id) {
+        for (let i = 0; i < state.friends.length; i++) {
           const friend = state.friends[i]
-          if(friend.id === id){
+          if (friend.id === id) {
             return friend
           }
         }
       }
     },
     //getters里的方法，可以使用别的store里的数据
-    showMessage(state){
+    showMessage(state) {
       const userStore = userStoreBuilder()
       return `name:${userStore.name} - count:${state.count}`
     }
   },
+
+  //pinia的actions可以做异步请求，没有mutations
+  actions: {
+    //无参
+    increment() {
+      this.count++
+    },
+    //参数是调用处传过来的值，不是state
+    incrementNum(num) {
+      this.count += num
+    },
+    //异步请求，调用处可以用Promise接收请求完成回调
+    async fetchHomeMultidata() {
+      const res = await fetch("http://123.207.32.32:8000/home/multidata")
+      const data = await res.json()
+
+      this.banners = data.data.banner.list
+      this.recommends = data.data.recommend.list
+      // 不返回任何值，等价于下面两行
+      // return undefined
+      // return Promise.resolve(undefined)
+    },
+    //异步请求，调用处可以用Promise接收请求完成回调
+    fetchHomeMultidata2() {
+      return new Promise(async (resolve, reject) => {
+        const res = await fetch("http://123.207.32.32:8000/home/multidata")
+        const data = await res.json()
+        this.banners = data.data.banner.list
+        this.recommends = data.data.recommend.list
+        resolve("bbb")
+      })
+    }
+  }
 })
 //store有三个核心概念：state、getters、actions,等同于组件的data、computed、methods
 //一旦store被实例化，你就可以直接在store上访问state、getters、actions中定义的任何属性
