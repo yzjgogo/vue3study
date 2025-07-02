d5cfad1b65d383a0c9987c8c08d3423d485f65f9
yzjgogo <yzjgogo@139.com>
2025-07-02 23:14:25 +0800

    提交一波代码

---
+++ react_scaffold/src/15_4_受控组件-form表单-select单选多选/App.jsx
@@ -0,0 +1,142 @@
+import React, { PureComponent } from 'react'
+
+export class App extends PureComponent {
+
+  constructor() {
+    super()
+
+    this.state = {
+      username: "",
+      password: "",
+      isAgree: false,
+      hobbies: [
+        { value: "sing", text: "唱", isChecked: false },
+        { value: "dance", text: "跳", isChecked: false },
+        { value: "rap", text: "rap", isChecked: false }
+      ],
+      fruit: ["orange"]//单选时'orange'
+    }
+  }
+
+  handleSubmitClick(event) {
+    // 1.阻止默认的行为
+    event.preventDefault()
+
+    // 2.获取到所有的表单数据, 对数据进行组件
+    console.log("获取所有的输入内容")
+    console.log(this.state.username, this.state.password)
+    const hobbies = this.state.hobbies.filter(item => item.isChecked).map(item => item.value)
+    console.log("获取爱好: ", hobbies)
+
+    // 3.以网络请求的方式, 将数据传递给服务器(ajax/fetch/axios)
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleAgreeChange(event) {
+    this.setState({ isAgree: event.target.checked })
+  }
+
+  handleHobbiesChange(event, index) {
+    const hobbies = [...this.state.hobbies]
+    hobbies[index].isChecked = event.target.checked
+    this.setState({ hobbies })
+  }
+
+  handleFruitChange(event) {
+    console.log('水果', event)
+    //event.target.selectedOptions是一个伪数组,Array.from可以将其转换为真正的数组
+    const options = Array.from(event.target.selectedOptions)
+    const values = options.map(item => item.value)//value属性就是apple、orange、banana
+    this.setState({ fruit: values })
+
+    // 额外补充: Array.from(可迭代对象)，可以将可迭代对象转换为数组
+    // Array.from(arguments,mapFn) 可以接收第二个参数，类似于map函数
+    const values2 = Array.from(event.target.selectedOptions, item => item.value)
+    console.log(values2)
+  }
+
+  render() {
+    const { username, password, isAgree, hobbies, fruit } = this.state
+
+    return (
+      <div>
+        <form onSubmit={e => this.handleSubmitClick(e)}>
+          {/* 1.用户名和密码 */}
+          <div>
+            <label htmlFor="username">
+              用户: 
+              <input 
+                id='username' 
+                type="text" 
+                name='username' 
+                value={username} 
+                onChange={e => this.handleInputChange(e)}
+              />
+            </label>
+            <label htmlFor="password">
+              密码: 
+              <input 
+                id='password' 
+                type="password" 
+                name='password' 
+                value={password} 
+                onChange={e => this.handleInputChange(e)}
+              />
+            </label>
+          </div>
+
+          {/* 2.checkbox单选 */}
+          <label htmlFor="agree">
+            <input 
+              id='agree' 
+              type="checkbox" 
+              checked={isAgree} 
+              onChange={e => this.handleAgreeChange(e)}
+            />
+            同意协议
+          </label>
+
+          {/* 3.checkbox多选 */}
+          <div>
+            您的爱好:
+            {
+              hobbies.map((item, index) => {
+                return (
+                  <label htmlFor={item.value} key={item.value}>
+                    <input 
+                      type="checkbox"
+                      id={item.value} 
+                      checked={item.isChecked}
+                      onChange={e => this.handleHobbiesChange(e, index)}
+                    />
+                    <span>{item.text}</span>
+                  </label>
+                )
+              })
+            }
+          </div>
+
+          {/* 4.select 
+            想把select设置为单选，则state中的fruit应该是一个字符串，例如this.state = {fruit: "orange"}，然后去掉multiple属性
+          */}
+          <select value={fruit} onChange={e => this.handleFruitChange(e)} multiple>
+            <option value="apple">苹果</option>
+            <option value="orange">橘子</option>
+            <option value="banana">香蕉</option>
+          </select>
+
+          <div>
+            <button type='submit'>注册</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
