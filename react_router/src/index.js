1506cc1e090b4de8b1438c5cae75e8f8e3bd9d82
yzjgogo <yzjgogo@139.com>
2025-07-18 10:34:55 +0800

    提交一波代码

--- react_router/src/index.js
+++ react_router/src/index.js
@@ -1,16 +1,36 @@
 // import { StrictMode } from "react"
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+/**
+ 回忆提示：
+ App.jsx、App函数组件-Routes路由.jsx、App类组件.jsx的区别
+ 最开始是用的App类组件.jsx，这是个类组件，但是讲到‘订单’、‘分类’的代码跳转路由时，发现类组件无法使用useNavigate，所以把‘App类组件.jsx’改造成了‘App函数组件-Routes路由.jsx’这样就可以使用useNavigate了，‘App类组件.jsx’作为备份放到那里。
+ 
+ ‘App函数组件-Routes路由.jsx’主要讲了通过Routes组件配置路由的方式，回忆的时候要把‘App函数组件-Routes路由.jsx’改成‘App.jsx’进行回忆
+
+ 后来讲到配置路由的时候，又把‘App函数组件-Routes路由.jsx’备份起来了，用的现在的App.jsx
+
+ 以后回忆的时候还是以‘App函数组件-Routes路由.jsx’为主
+  
+ */
+
 //react路由使用步骤1：npm install react-router-dom
 //BrowserRouter：是history模式的路由
 //HashRouter：是hash模式的路由
 //这里我们以HashRouter为例
 // import { BrowserRouter,HashRouter } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom'
+import { Suspense } from "react"
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 ////react路由使用步骤2：用HashRouter包裹App组件
 root.render(
   <HashRouter>
-    <App />
+    {/* react路由使用步骤9路由配置4：因为在E:\web\vue3\vue3study\react_router\src\router\index.js中About和Login使用了懒加载，所以这里用Suspense:
+    https://blog.csdn.net/weixin_40629244/article/details/148502371
+    */}
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <App />
+    </Suspense>
   </HashRouter>
 )
--- react_router/src/router/index.js
+++ react_router/src/router/index.js
@@ -2,8 +2,6 @@ import Home from '../pages/Home'
 import HomeRecommend from "../pages/HomeRecommend"
 import HomeRanking from "../pages/HomeRanking"
 import HomeSongMenu from '../pages/HomeSongMenu'
-// import About from "../pages/About"
-// import Login from "../pages/Login"
 import Category from "../pages/Category"
 import Order from "../pages/Order"
 import NotFound from '../pages/NotFound'
@@ -11,10 +9,18 @@ import Detail from '../pages/Detail'
 import User from '../pages/User'
 import { Navigate } from 'react-router-dom'
 import React from 'react'
-
+// import About from "../pages/About"
+// import Login from "../pages/Login"
+////react路由使用步骤9路由配置3：也可以使用懒加载来加载路由，这里的import是webpack的特性。和vue一样，懒加载也是，在你'npm run build'打包时，打出的包里，这个组件是一个单独的js文件，在使用时才加载。可以配合Suspense使用，Suspense用于加载过程中的等待过度画面
 const About = React.lazy(() => import("../pages/About"))
 const Login = React.lazy(() => import("../pages/Login"))
-
+/*
+  react路由使用步骤9路由配置1：
+  react也支持像vue一样的路由配置方式，
+  在react的router5.x中，需要引入依赖react-router-config实现路由配置
+  现在在react-router-dom中提供了useRoutes，直接就能实现路由配置
+*/
+//react路由使用步骤9路由配置2：定义数组配置所有的路由，其中的path就是‘App函数组件-Routes路由.jsx’中Route的path属性，element就是Route的element属性
 const routes = [
   {
     path: "/",
