a661ba068e8f27e66f709da48f7131d5a90a9edd
yzjgogo <yzjgogo@139.com>
2025-08-05 19:25:32 +0800

    提交一波代码

--- react_hook/src/03_useState的使用/App.jsx
+++ react_hook/src/03_useState的使用/App.jsx
@@ -1,6 +1,9 @@
 import React, { memo, useState } from 'react'
 
 const App = memo(() => {
+  //useState(()=>{return "Hello World"})
+  // useState(() => "Hello World") // 这种方式是懒初始化，对性能优化很重要，因为这个函数只会在组件的初次渲染时执行，而不会在重新渲染时重复执行。
+  //参考：E:\web\vue3\vue3study\react_hook\src\12_自定义Hooks\hooks\useLocalStorage.js
   const [message, setMessage] = useState("Hello World")
   const [count, setCount] = useState(100)
   const [banners, setBanners] = useState([])
--- react_hook/src/12_自定义Hooks/hooks/useLocalStorage.js
+++ react_hook/src/12_自定义Hooks/hooks/useLocalStorage.js
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useState } from "react"
 
 function useLocalStorage(key) {
+  //接收了一个函数而非直接值作为参数，这种方式称为"懒初始化"（lazy initialization）。这对性能优化很重要，因为这个函数只会在组件的初次渲染时执行，而不会在重新渲染时重复执行。
   // 1.从localStorage中获取数据, 并且数据数据创建组件的state
   const [data, setData] = useState(() => {
     const item = localStorage.getItem(key)
